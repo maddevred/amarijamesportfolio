13bc9b880eab7cd8806c6966fdda23ca149158a6
Amari James <mar.james1998@gmail.com>
2021-02-23 11:56:53 -0500

    plswork

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Section from "./Components/Section";
+import dummyText from "./DummyText";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Section
+          title="About Me"
+          subtitle={dummyText}
+          dark={true}
+          id="section1"
+        />
+        <Section
+          title="Skills"
+          subtitle={dummyText}
+          dark={false}
+          id="section2"
+        />
+        <Section
+          title="Projects"
+          subtitle={dummyText}
+          dark={true}
+          id="section3"
+        />
+        <Section
+          title="Contact Me"
+          subtitle={dummyText}
+          dark={false}
+          id="section4"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
