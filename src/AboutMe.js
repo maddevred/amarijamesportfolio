5260977bbf32c32be66f1b4a8533c99be951e0ec
Amari James <mar.james1998@gmail.com>
2021-02-23 19:54:11 -0500

    mostlyworking

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,7 +2,10 @@ const aboutMe =
 <html>
 <body>
 
-<img id="img1" src="https://gm1.ggpht.com/s5ZtmUbtbepOC-6sG-AJ0jVaxxOnGQY35VAfwnBJ-hpulhzmCwYoxSfsxE0gUUS5oB6_WwQPqnDOq9TwQCpgan8F8rfH3WHOpirE6t1BJMIR2Gm4VSN-ZXbzXrFcY7N7_tucAIJbJQ3E1b5jujXtnEF-Ddd-xLvGbuKyrOHqiwhcJHNn91bEFA5psJH6T97hyXAq7gMAgneSA8_kHFDeLLPaKfbNQRd7dmZzp3EYmL7-j7IwM5tEbXcfM-HPM5w_zdJcXn4rlY9FLVBTqv_iT1HGEOWjtpe9WQsTkQbhcERqawa4M_sZT-sqo7FKFQKUHWMBlknbIsO1HdyvwG4ijoNSKD0u_Gm5FR0eKK9fxxwBuynFUISht3AEFtHnwME4wW2AXrTYgtiRlG1pGfPeIA4CydiRBJ5dhG2Q6RSChTpVcFpPx9qjuxFtoQoxhy8gTlNFkImhZmKKn-RU1G9T88Sukr29Ec2aRNke8cs5eeMHmsiRv5EnugjZK_Xshc7nuWgDt_5SMnXHP6_Z_b6deCh66cWj-f4jlTgiZYE_Qj_3EJwP4XFziGacK5a6pdueHCtvSxBFbEwaW0YaHA316Go1ftx2wI2PcgRxk5aezEOFxg9EFsJSGgwRmlD9_tINCBvXpT47duECWSwMPK2ni2Qg2_RySMd1UoewyIAhPwbLtrZjF_qZ7vPQzoURkNCD0pByFdiAy2md4chfSOfyp34BHc3k4IxH4Sx3ay84k8DDxTr5tMtMU0s0-jEvy7OCdw=s0-l75-ft-l75-ft">
+<br></br>
+<br></br>
+
+<img id="img1" src="https://scontent.ftpf1-1.fna.fbcdn.net/v/t1.0-9/122962758_977055522816400_7039963796302645343_n.jpg?_nc_cat=103&ccb=3&_nc_sid=8bfeb9&_nc_ohc=mqZ9jBPBhM4AX_0iy8A&_nc_ht=scontent.ftpf1-1.fna&oh=ed6886a72e9a67dfcc65ad72883fe46a&oe=6059D4E6">
 
 </img>
 
@@ -18,9 +21,8 @@ Graduated from General Assembly's Software Engineering Immersive Program.</p>
 <br></br>
 <br></br>
 <br></br>
-<br></br>
 
-
+<a id="resume" href="https://docs.google.com/document/d/1NNlxgewo6mfE29mfXoArziVPDmiZNKF9vznYNRwRBx4/edit?usp=sharing" target='_blank'>view my resume here</a>
 
 <script>
 
--- src/Projects.js
+++ src/Projects.js
@@ -3,10 +3,27 @@ const projects =
 <html>
 <body>
 
-<h1>My First Web Page</h1>
-<p>My First Paragraph</p>
+<div>
+    <h1>HZ HQ</h1>
+    <img class="hzhq" src="https://healthcare-india.org/wp-content/uploads/2019/06/19cover-1-696x465.jpg"></img>
+    <br></br>
+    <button onclick="window.open('https://hzfreq.herokuapp.com/', '_blank');">view WEBSITE here</button>
+    <p>Website application to find all hertz frequencies and their properties as well as links to listen to them. Built using Express, Node.js, JS, and HTML. </p>
+    <br></br>
 
-<p id="demo"></p>
+    <h2>OUID</h2>
+    <img class="ouid" src="https://media.self.com/photos/5e96135a2c928600095ebd3c/master/pass/weed_marijuana.jpeg"></img>
+    <br></br>
+    <button onclick="window.open('https://github.com/marjames98/projectfourOUID', '_blank');">view REPO here</button>
+    <p>Website application on Tetrahydrocannabinol strains and information. Built using Express, Node.js, mongoDB, JS, and HTML. Attempt at API routing. Under constant construction due to errors in API call.</p>
+    <br></br>
+  
+    <h3>Irezumi</h3>
+    <img class="irezumi" src="https://lh3.googleusercontent.com/proxy/dxmfRZfOXSwosYJQJwRQ7IZtVDJR3UH13oqBzK3Y-7EO84ZQFTTsQ8IKH2ZLt39AzCsK99Q_PGN09vSkIc5RpoF8I2HZIn9BigdHLajf4LdIUFMmXLvT6u8A96TyZcKEdGouXAhRygliAQAB6e94R2dtZsSZmdE"></img>
+    <br></br>
+    <button onclick="window.open('http://irezumi-tattoo.herokuapp.com/', '_blank');">view PROJECT here</button>
+    <p>Team Project. Full-stack application to find tattoo inspiration as well as local tattoo shops, both of which can be saved and added to a user profile for future use. Additionally, users can see tattoos they are interested in on a hypothetical body part of their choosing. Built using mongoDB, React, Axios, Cloudinary and Bootstrap Styling, as well as API routing.  Deployed and hosted on Heroku. </p>
+</div>
 
 <script>
 document.getElementById("demo").innerHTML = 5 + 6;
