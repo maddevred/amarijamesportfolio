4db1832dfbb3e0eb775f00887476700151f74d9e
Amari James <mar.james1998@gmail.com>
2021-06-19 13:15:15 -0400

    inprogress

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,7 +4,7 @@ const aboutMe =
 
 <br></br>
 <br></br>
-{/* 
+
 <img class="img1" src="https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/186297485_1104822086706409_5390037232218721306_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=5Yfbs1B1NpMAX-jWdSf&_nc_oc=AQkKAwPin66FjQNBc-_qsjIv_5zbILuB01eQumNeypTLeHfc6K8H2BlBIUvZjcGwhtZClGUBW6S2DGXTy1nkLXiH&_nc_ht=scontent-atl3-2.xx&oh=079620e3f95b952677500f16836326d8&oe=60C3ADBC"></img>
 
 
@@ -13,12 +13,10 @@ const aboutMe =
 <br></br>
 <br></br>
 
-<h1>Late @ Nite Apps 'N Sites</h1> */}
+<h1>Late @ Nite Apps 'N Sites</h1> 
 
 <br></br>
 
-<strong>*** WEBSITE CURRENTLY UNDER CONSTRUCTION!!! <br></br> PLEASE CHECK BACK FOR UPDATES, FIXES, AND NEW FEATURES :) ***</strong>
-{/* 
 <p><em>Late @ Nite Apps 'N Sites</em> is a mobile web and application construction company that is at your service 24 hours a day, five days a week. 
     Owned and operated by a detail-oriented full-stack engineer specializing in adaptive, polished, and prompt professional software development. 
     We look forward to working with you!
@@ -29,7 +27,7 @@ const aboutMe =
 <br></br>
 
 <p><em>Scan below to view service pricing!</em></p>
-<img class="img2" src="https://gm1.ggpht.com/xau8P080zbx4jQpO6EAuaSK2MA5LS-BShoKl1cxpQWqSwXfx5Ni81cly3TOyDJ7gTub3mQeaM09vZ9GLfTUYosJWsTPapD7s66KmwFbPNIyeWXWMXJDqDi2odn7dgunw6iYLW6GLcZFqKzx-ipiGHt9nCPW4GKuE6zSFkVYyWgCkjiClgT41p5WpOHSVIBfyMl3FgC93tEpR_IPSJYte7me0VjwkW8sECsNWZS40FcwJZUbow0o-5li-NxzkAoelU5AH25lWezipe7DkvrnIvlgTmCVT9b6xbOtK9yFeuVFc9Zm5aiFntgLe08ue8ndwy62v8k4Z_fbqV4-u-OE3Ie1uiT3147NQ1BOwA83tcfgDe-J3eZps_1XyiZaCqdDl2UzHfroc6nsgpHhX5JVTYpTO9sMh23cVkKdOP0a1lbY9df62bjLGLk_g4hTGh0ylDqZH8aG2O4GCzjgyeRcQ17UGEjQabez7GMD1f3DcyktbWLJzXdVeC2IM10tcjeehUBc4EjjEG9d73_BW-teP0tfCCwvkBS72leuVPtQuENZu6NnvuI8YUzqgYRj-psN9-y_GEi0Kt2Ypw9_yf61LYSTWtsw9CEAySimiBasFc74XePmoD_EqFV788sqyalicu1W_eVJZhd5opuaobMCijxdlP4gTo3JtjR5gF0n1gi3N6BQEZH0CR3rIAosaZx3sGiGVwE_SyFQ983O1LSTOtXgvN4DsCBeqNFl0TCd7DquD_1BTJ8gob6lK8ndHXvZc1w=s0-l75-ft-l75-ft"></img> */}
+<img class="img2" src="https://gm1.ggpht.com/xau8P080zbx4jQpO6EAuaSK2MA5LS-BShoKl1cxpQWqSwXfx5Ni81cly3TOyDJ7gTub3mQeaM09vZ9GLfTUYosJWsTPapD7s66KmwFbPNIyeWXWMXJDqDi2odn7dgunw6iYLW6GLcZFqKzx-ipiGHt9nCPW4GKuE6zSFkVYyWgCkjiClgT41p5WpOHSVIBfyMl3FgC93tEpR_IPSJYte7me0VjwkW8sECsNWZS40FcwJZUbow0o-5li-NxzkAoelU5AH25lWezipe7DkvrnIvlgTmCVT9b6xbOtK9yFeuVFc9Zm5aiFntgLe08ue8ndwy62v8k4Z_fbqV4-u-OE3Ie1uiT3147NQ1BOwA83tcfgDe-J3eZps_1XyiZaCqdDl2UzHfroc6nsgpHhX5JVTYpTO9sMh23cVkKdOP0a1lbY9df62bjLGLk_g4hTGh0ylDqZH8aG2O4GCzjgyeRcQ17UGEjQabez7GMD1f3DcyktbWLJzXdVeC2IM10tcjeehUBc4EjjEG9d73_BW-teP0tfCCwvkBS72leuVPtQuENZu6NnvuI8YUzqgYRj-psN9-y_GEi0Kt2Ypw9_yf61LYSTWtsw9CEAySimiBasFc74XePmoD_EqFV788sqyalicu1W_eVJZhd5opuaobMCijxdlP4gTo3JtjR5gF0n1gi3N6BQEZH0CR3rIAosaZx3sGiGVwE_SyFQ983O1LSTOtXgvN4DsCBeqNFl0TCd7DquD_1BTJ8gob6lK8ndHXvZc1w=s0-l75-ft-l75-ft"></img>
 
 
 <script>
--- src/Skills.js
+++ src/Skills.js
@@ -3,9 +3,7 @@ const skills =
 <html>
 <body>
 
-<strong>*** WEBSITE CURRENTLY UNDER CONSTRUCTION!!! <br></br> PLEASE CHECK BACK FOR UPDATES, FIXES, AND NEW FEATURES :) ***</strong>
-
-{/* <img class="mongoose" src="https://cms-assets.tutsplus.com/uploads/users/34/posts/29527/preview_image/mongoose.jpg"></img>    
+<img class="mongoose" src="https://cms-assets.tutsplus.com/uploads/users/34/posts/29527/preview_image/mongoose.jpg"></img>    
 
 <img class="linux" src="https://mewallpaper.com/thumbnail/abstract/6424-linux-tux-logo-hd-free-wallpaper-hq.jpg"></img>
 
@@ -45,7 +43,7 @@ const skills =
 
 <br></br>
 
-<img class="jade" src="https://jadeframework.dev/jade_logo-full-meta.png"></img> */}
+<img class="jade" src="https://jadeframework.dev/jade_logo-full-meta.png"></img>
 
 <script>
 document.getElementById("demo").innerHTML = 5 + 6;
