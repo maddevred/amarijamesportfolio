273f89d20b84dcc234d54534eeba6ce0e5659c7c
Amari James <mar.james1998@gmail.com>
2021-06-19 14:02:11 -0400

    addedpic

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,7 +5,7 @@ const aboutMe =
 <br></br>
 <br></br>
 
-<img class="img1" src=""></img>
+<img class="img1" src="https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/186297485_1104822086706409_5390037232218721306_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=zeJbZoCWOC0AX8AqIMQ&_nc_oc=AQnYYbwCkUMxhTdksfqF0Ipib-Lb9Jgsrb5bM9DXSzOxLx0s7Jdqe8KpAG8qM2hpgTUPB4bfj36AeQyIqahtchvb&_nc_ht=scontent-atl3-2.xx&oh=db994c233977552f13e1fc64e5ec03a9&oe=60D37FBC"></img>
 
 
 <br></br>
@@ -14,16 +14,8 @@ const aboutMe =
 <br></br>
 
 <h1>Amari James</h1> 
-
-<br></br>
-
 <p> Specializing in adaptive, polished, and prompt professional software development.</p>
-
-<br></br>
-<br></br>
-<br></br>
-
-<p><em>Click <a href="https://docs.google.com/document/d/e/2PACX-1vSlAmPLslV1XgLziz6xSllcTpQcchXG85MUm8HxRY4UY32GM3XbncFDTLVYn8XKeTuoln0gWm2e-Dlr/pub">here</a> to view resume!</em></p>
+<p><em>Click <a href="https://docs.google.com/document/d/e/2PACX-1vSlAmPLslV1XgLziz6xSllcTpQcchXG85MUm8HxRY4UY32GM3XbncFDTLVYn8XKeTuoln0gWm2e-Dlr/pub"><strong>here</strong></a> to view resume!</em></p>
 
 <script>
 
