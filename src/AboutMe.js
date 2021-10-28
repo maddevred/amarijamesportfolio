6e4ab740fd4bbc9098ebab0fee219fa76136006b
marjames98 <marjames1998@gmail.com>
2021-10-28 18:41:10 -0400

    almostdone

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,7 +5,7 @@ const aboutMe =
 <br></br>
 <br></br>
 
-<img class="img1" src="https://lh3.googleusercontent.com/zbeu04jp3rzwf53U6eW_hem3zHvpLU9iPhS_HSmLPedKc9SGeEk3tKkdPMxb03n25VMH8mMnAu-df0hs51JqYj9RCdc53F-48geDGO6tf8F3lKXBp2YxERQTKI86fz85o-jdKHU7S8gn_hQu5gMlooSG_-AmRfRez6_xyF8iKIFkleEgVO99yBR7zJcDrFN9BemnLMZLiUGiUQmSCeQcOdi3nIULpfU7GFAVMBdKnSXdZP0aR6RRfRdmIdRy2FMDX_qtK_7rriLNeee_YHAC4yIok-LOByHYwNo21w8cBeReFqo0cHz8tyDBoLNIt4sa6GLtiw85PQDd85scPfq1jPpnMsLIMvxQQUmkmETuGZb0jHCfWTaSLw1LvUuMnD-rAE9UInkjwJLVtnk_uGymo-8dpFlEGj2JsjJHoBfGUU2ZyXkB-dvCDACOMR5GmQLR9ljG4lHc2jycZ9v21IjebIqz5uxQYSH3AL4xReExXksmWxWEcs4q03FJdLfK9C3S56Qn9CHVSJdhCVLXyvLViKnv3NaiwO4PFiyjMJLjhDAmR_Q5daoSjZet6seqIx4LsxP2xdPMp3iFR1YpC7LvWv1cYh1mxYao5UJQTpXLI_kX3_F3QfmSG_EquGL88bZlokuLI9OaQ4PgVUeWz6wUOm9gcakXmjIr7gW-0uYi6jX6L7dWu7YwFxVXlMlsKLeqF0z04F7Ptxyk3yg6xAXq-JE=w154-h207-no?authuser=0" alt="A current headshot."></img>
+<img class="img1" src="https://lh3.googleusercontent.com/fife/AAWUweXCZP46DWOhJZ5qnPOdh1-j55n3FOXOz54165UU-uWKXprEO2YlRm1DvYvdNI2kzt9NXDYJFRKeugFdcyLtXDWwqnOs3NBIwKnRTNssdWEVbECfzYdNwyITjQ7hvh2aN-iU4tDQKSU45cwqegJeKYlfm7Rh9-ZSYxy7BsIEaNae8aMwcoVgnF8AjUMeSPJ6JbSGqa5WnXN0TsJUvZr-1npFhlxgRYjHVPILtpiJahIlmNrlfNCITJ-XdlTn_0JmotrRtGhLnKvRnMKtKBt_SEE9XCyPPOVFeDVjshjveb_W6Y1D1d9xNpKW3HBpqMEtUHFOYvggi4fRgwVjsjpA-wuuiAS4bHCwXSv1tNqar21L3IIXFDfeakJnmJSRuNoxwdfnD9PFBC3fIplhSwBgrvVL3h-pQ_A6j6ReVqPTrAF24nYB35nyR-YwDr982QRhZmMo0QvIsaiET46dvuzSq0iYlIz0H9JkLdr4UKJ-H8gdN1ahQhCjmlL-fgzX4cPRijeffprKMryvSaJZZuamiazhSwczbMR9KNI1_e3K79SZ4FQbwRw4M_rT9ofADitLs10GPXFqMrM05XNwIFzRIarSelnnwHmiWxp9NUJdTkEHGUcofqqxhLS2QOajJAQ8kO46qCok2jIwI8QIvMO8585GjAhmcRwBPdNSrCaW4okhJdR3Nb4PC6P8uMdGQGle4sdT_p3yzAdDe2R8eQHI9F8=w155-h194-no?authuser=0" alt="A current headshot."></img>
 
 
 <br></br>
@@ -15,7 +15,7 @@ const aboutMe =
 
 <h1>Amari James</h1> 
 <p> Specializing in adaptive, polished, and prompt professional software development.</p>
-<p><em>Click <a target="_blank" href="https://docs.google.com/document/d/e/2PACX-1vSlAmPLslV1XgLziz6xSllcTpQcchXG85MUm8HxRY4UY32GM3XbncFDTLVYn8XKeTuoln0gWm2e-Dlr/pub"><strong>here</strong></a> to view resume!</em></p>
+<p><em>Click <a target="_blank" href="https://www.canva.com/design/DAEnH9EL7fU/4dwcBjK2w7gw2m9-f_PNsQ/view?utm_content=DAEnH9EL7fU&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"><strong>here</strong></a> to view resume!</em></p>
 
 <script>
 
--- src/ContactMe.js
+++ src/ContactMe.js
@@ -21,7 +21,7 @@ const contactMe =
 <br></br>
 
 <img class="email" src="https://images.indianexpress.com/2020/10/Gmail-new-logo.jpg"></img>
-<p>mar.james1998@gmail.com</p>
+<p>marbusiness98@gmail.com</p>
 <br></br>
 
 <img class="phone" src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/phone-512.png"></img>
