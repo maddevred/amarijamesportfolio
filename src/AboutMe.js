f942cf5dc6651ff27ff1b8b5fb33008ca73df703
marjames98 <mar.james1998@gmail.com>
2021-11-09 11:50:53 -0500

    alldonefornow

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,7 +5,7 @@ const aboutMe =
 <br></br>
 <br></br>
 
-<img class="img1" src="https://lh3.googleusercontent.com/fife/AAWUweUD4xBDC9dxUh-zti88_UOjjdSoIklMjb-5SgPmMAvnwuqMbL8vAVMT_5CQvXd8ZhFvxyvniH9bSFvjX9wbfwu6RJgR4Jj8Nvg8cSc1E3FHICLCt7Zktx7JQwv3V_pXz8RkywMhkxwCNfRBPnIpawJhR015mUzf-cjpa1_l6VTCqM5XFCmqy4bNdwyKq46wHgdTCPU50LPtc0wE6qSWCQ_ShdwjF7lZVrRWEdvfwkCeNrycLDPJCr028rUGkwr04Hzz1goaCB8R7F0VhbdqsVehRh-hH-8DsYRa2B3H5xEZBDgJIy5b5hAiRLUtJRBpeRc7VFZMtumBOphH9nATtcgNFeLr4XaAheikYe_BNzI0nadG069zsQoJTXrmTtEVKSHWWhjyVrv-O9ZOAV20lcC-BZBeqX1djVCPECeAh0oTbeDw2-Vhn5lL-def9HU5pcN1ez8ycHKi6il9EXmXHliPcSh23Q8BN5jPPRlM3YL7rQFRgai1DBpeA74TqpVs7dc2amMJCh8jlPmm64DcK5yHenJIcjRB0RDxpz4V__GupxrnnpOj6ec0yz5N39xU7dtKo0CSYFaZ-dQg6mKuiyqREHvgB2NmNO1Yk65vVK4CyteUkYH2sOhk6xCerLVsuoeGFVnKFJX_dLJb9nw6MVnFyf96FatCX9x24DpEqCDGRyhZILbujAFM8zZ_pyk7tMCeHwAAQRbsPbdJ2u98mh4=w155-h194-no?authuser=0" alt="A current headshot."></img>
+<img class="img1" src="https://lh3.googleusercontent.com/fife/AAWUweVweYsLEHcst1H7OIXhvDFC8ZtlLtga5_qfBFEB9rgp-B0K0oZSEqr668Q3BRBgTXO3HYmimxLg549PD_MaW9LAU7fb736lSKgyhlhYR7JNRoEm_nirAv63s8ldYR5XqVkUk7mgkCdC4fLgpEEZOaogWc8V7P0v1tDfFkOcDa8lotNtltPAc2WfdlDUVdtX_RLfzmBwEYVDNcUnF9C7cX7A2eNyD3IZCwUrP36uGID9PHT3B3Ao-VWKpZ0KrWhQWjQP237eyxcfeNFv06BlEgNl6oGAMTyPdvRLdJ8A17fMUgn2ZFZ8TMoc_eZScDrkVK2gQi6Ly7Dr7UgbtU2pvKiYwTKcIXNSCUJH5UATk3qAPlrIJGMHWnQ6R8YXL3EKcPUtgWTopRMkeIDErt2XyUWWkts-3vOvSZYuG9qvfg_d2CfjMDYM4KVRcCPlEgSCAwRTEjeHCoPtg-M87m3V0cn1OfxVXn-wf7ba37ZKF1oTZjKBobGoAMv17_RPk_XwOgn-fDz0ov6SV11O9AtLaw58M9Fp6jtp9GqNSMs1s88YhRe6oRoPNvIuPTAdEXVDMdh9UbcWJFgUhdZyG2FthlbM9xwIg4mqf4PCpHmN89lNMIBalYajVDogFlar2xUfpCb4x9i1u6v4CGFSmU_DPTVKQu-guYbZi44HCBC9kVMlbWymqrC4YUm_tJExKVYcacMorEk6bGz-_WYbTKCyckk=w155-h194-no?authuser=0" alt="A current headshot."></img>
 
 
 <br></br>
@@ -15,7 +15,7 @@ const aboutMe =
 
 <h1>Amari James</h1> 
 <p> Specializing in adaptive, polished, and prompt professional software development.</p>
-<p><em>Click <a target="_blank" href="https://www.canva.com/design/DAEnH9EL7fU/4dwcBjK2w7gw2m9-f_PNsQ/view?utm_content=DAEnH9EL7fU&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"><strong>here</strong></a> to view resume!</em></p>
+<p><em>Click <a target="_blank" href="https://photos.app.goo.gl/YecLDw5HGFJ23aLu6"><strong>here</strong></a> to view resume!</em></p>
 
 <script>
 
