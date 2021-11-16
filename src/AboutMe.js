339550b52b3da4cdef4afc3eadbb0dd5c9965bd9
marjames98 <mar.james1998@gmail.com>
2021-11-15 19:31:05 -0500

    almostdone

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -3,19 +3,20 @@ const aboutMe =
 <body>
 
 <br></br>
-<br></br>
 
 <img class="img1" src="https://lh3.googleusercontent.com/fife/AAWUweVweYsLEHcst1H7OIXhvDFC8ZtlLtga5_qfBFEB9rgp-B0K0oZSEqr668Q3BRBgTXO3HYmimxLg549PD_MaW9LAU7fb736lSKgyhlhYR7JNRoEm_nirAv63s8ldYR5XqVkUk7mgkCdC4fLgpEEZOaogWc8V7P0v1tDfFkOcDa8lotNtltPAc2WfdlDUVdtX_RLfzmBwEYVDNcUnF9C7cX7A2eNyD3IZCwUrP36uGID9PHT3B3Ao-VWKpZ0KrWhQWjQP237eyxcfeNFv06BlEgNl6oGAMTyPdvRLdJ8A17fMUgn2ZFZ8TMoc_eZScDrkVK2gQi6Ly7Dr7UgbtU2pvKiYwTKcIXNSCUJH5UATk3qAPlrIJGMHWnQ6R8YXL3EKcPUtgWTopRMkeIDErt2XyUWWkts-3vOvSZYuG9qvfg_d2CfjMDYM4KVRcCPlEgSCAwRTEjeHCoPtg-M87m3V0cn1OfxVXn-wf7ba37ZKF1oTZjKBobGoAMv17_RPk_XwOgn-fDz0ov6SV11O9AtLaw58M9Fp6jtp9GqNSMs1s88YhRe6oRoPNvIuPTAdEXVDMdh9UbcWJFgUhdZyG2FthlbM9xwIg4mqf4PCpHmN89lNMIBalYajVDogFlar2xUfpCb4x9i1u6v4CGFSmU_DPTVKQu-guYbZi44HCBC9kVMlbWymqrC4YUm_tJExKVYcacMorEk6bGz-_WYbTKCyckk=w155-h194-no?authuser=0" alt="A current headshot."></img>
 
+<div class="dark fire">
+        <h1 class="Blazing" contenteditable="true">Amari James</h1> 
+</div>  
 
-<br></br>
-<br></br>
-<br></br>
-<br></br>
+{/* <div class="dark fire">
+        <h4 class="Subtitles" contenteditable="true">Full-Stack Software Developer</h4>
+</div>   */}
 
-<h1>Amari James</h1> 
+<strong><p>Full-Stack Software Developer</p></strong>
 <p> Specializing in adaptive, polished, and prompt professional software development.</p>
-<p><em>Click <a target="_blank" href="https://www.canva.com/design/DAEu0THil84/share/preview?token=ZkUU3OCaE8X5rBUw5D3mGA&role=EDITOR&utm_content=DAEu0THil84&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton"><strong>here</strong></a> to view resume!</em></p>
+<p><em>Click <a target="_blank" href="https://www.canva.com/design/DAEu0THil84/view"><strong>here</strong></a> to view resume!</em></p>
 
 <script>
 
